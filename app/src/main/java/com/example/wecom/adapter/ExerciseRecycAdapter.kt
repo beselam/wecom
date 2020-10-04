05b88244a801df1df72ddf50a1aa388b7dfb04ee
Beselam-Asefa <gbeselam@gmail.com>
2020-10-04 18:26:27 +0300

    firebase auth finished

---
+++ app/src/main/java/com/example/wecom/adapter/ExerciseRecycAdapter.kt
@@ -0,0 +1,72 @@
+package com.example.wecom.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wecom.R
+import com.example.wecom.db.Exercise
+import java.text.SimpleDateFormat
+import java.util.*
+/*
+
+class  ExerciseRecycAdapter : RecyclerView.Adapter<ExerciseRecycAdapter.RunViewHolder>() {
+
+    inner class RunViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    val diffCallback = object : DiffUtil.ItemCallback<Exercise>() {
+        override fun areItemsTheSame(oldItem: Exercise, newItem: Exercise): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Exercise, newItem: Exercise): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+
+    val differ = AsyncListDiffer(this, diffCallback)
+
+    fun submitList(list: List<Exercise>) = differ.submitList(list)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RunViewHolder {
+        return RunViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_run,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: RunViewHolder, position: Int) {
+     */
+/*   val run = differ.currentList[position]
+        holder.itemView.apply {
+
+            val calendar = Calendar.getInstance().apply {
+                timeInMillis = run.timestamp
+            }
+            val dateFormat = SimpleDateFormat("dd.MM.yy", Locale.getDefault())
+            tvDate.text = dateFormat.format(calendar.time)
+
+            val avgSpeed = "${run.avgSpeedInKMH}km/h"
+            tvAvgSpeed.text = avgSpeed
+
+            val distanceInKm = "${run.distanceInMeters / 1000f}km"
+            tvDistance.text = distanceInKm
+
+            tvTime.text = TrackingUtility.getFormattedStopWatchTime(run.timeInMillis)
+
+            val caloriesBurned = "${run.caloriesBurned}kcal"
+            tvCalories.text = caloriesBurned
+        } *//*
+
+    }
+}
+*/

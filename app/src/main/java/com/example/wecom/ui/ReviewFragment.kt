b661fb4569bb68ff21c70b90d4d689e8fac77bd3
nebi <snoopafr@gmail.com>
2020-10-08 16:52:45 +0300

    graph part has been done

--- app/src/main/java/com/example/wecom/ui/ReviewFragment.kt
+++ app/src/main/java/com/example/wecom/ui/ReviewFragment.kt
@@ -1,20 +1,76 @@
 package com.example.wecom.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.wecom.R
 import com.example.wecom.viewmodel.MainViewModel
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import com.google.android.material.tabs.TabLayout
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.android.synthetic.main.bottom_sheet.*
-
+import kotlinx.android.synthetic.main.fragment_review.*
+@AndroidEntryPoint
 class ReviewFragment :Fragment(R.layout.fragment_review){
+
     private val viewModel: MainViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupBarChart()
+        subscribeToObservers()
+    }
+
+    private fun setupBarChart(){
+        barChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            setDrawLabels(false)
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+
+        barChart.axisLeft.apply {
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+
+        barChart.axisRight.apply {
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+
+        barChart.apply {
+            description.text = "Calories burned over distance covered"
+            legend.isEnabled = false
+        }
+    }
+
+    private fun subscribeToObservers(){
+        viewModel.getAllExersiseSortedByDate().observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val allAvgSpeed = it.indices.map { i -> BarEntry(i.toFloat(), it[i].averageSpeed) }
+                val bardataset = BarDataSet(allAvgSpeed,"Avg Speed Over Time").apply {
+                    valueTextColor = Color.WHITE
+                    color = ContextCompat.getColor(requireContext(),R.color.colorAccent)
+                }
+                barChart.data = BarData(bardataset)
+                barChart.invalidate()
+
+            }
+        })
 
     }
 
